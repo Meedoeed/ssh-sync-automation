bee40de045eff949b644ce07e9758a707f789158
Meedoeed <skykk27@gmail.com>
2026-04-08 22:10:24 +0300

    added logger and graceful shutdown stub

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,11 +1,30 @@
 package main
 
 import (
+	"context"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+
 	"github.com/Meedoeed/ssh-sync-automation/internal/config"
+	"github.com/Meedoeed/ssh-sync-automation/internal/infrastructure/logger"
 )
 
 func main() {
 	cfg := config.Load()
-	// Инициализация логера
+	logger.Init(cfg.Log.Level, true) // в разработке true, в проде false
+	logger.Get().Info().Msg("Starting SSH-SYNC-AUTOMATION service")
 	// Запуск http=сервера
+
+	// TODO http-server
+
+	// сразу сделал задел на graceful shutdown сервиса - пока что _=ctx - заглушка
+	quit := make(chan os.Signal, 1)
+	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+	_ = ctx
+
+	logger.Get().Info().Msg("Service stopped")
 }
