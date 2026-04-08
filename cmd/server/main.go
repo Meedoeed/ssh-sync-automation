f99441b805d2a4f6d5bd51a6564faba9c387501f
Meedoeed <skykk27@gmail.com>
2026-04-08 23:30:00 +0300

    updated with http-server and graceful shutdown

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -9,22 +9,32 @@ import (
 
 	"github.com/Meedoeed/ssh-sync-automation/internal/config"
 	"github.com/Meedoeed/ssh-sync-automation/internal/infrastructure/logger"
+	"github.com/Meedoeed/ssh-sync-automation/internal/server"
 )
 
 func main() {
 	cfg := config.Load()
 	logger.Init(cfg.Log.Level, true) // в разработке true, в проде false
 	logger.Get().Info().Msg("Starting SSH-SYNC-AUTOMATION service")
-	// Запуск http=сервера
 
-	// TODO http-server
+	httpServer := server.NewHTTP(cfg)
 
-	// сразу сделал задел на graceful shutdown сервиса - пока что _=ctx - заглушка
+	go func() {
+		if err := httpServer.Start(); err != nil {
+			logger.Get().Fatal().Err(err).Msg("Failed to start HTTP server")
+		}
+	}()
+
 	quit := make(chan os.Signal, 1)
 	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
+	<-quit
+
 	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
 	defer cancel()
-	_ = ctx
+
+	if err := httpServer.Shutdown(ctx); err != nil {
+		logger.Get().Error().Err(err).Msg("HTTP server shutdown error")
+	}
 
 	logger.Get().Info().Msg("Service stopped")
 }
