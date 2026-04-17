121d12c2d0c912a491380964b13d1d295ec217a9
Meedoeed <skykk27@gmail.com>
2026-04-17 21:34:34 +0300

    added server handler support

--- internal/server/http.go
+++ internal/server/http.go
@@ -1,4 +1,3 @@
-// internal/server/http.go
 package server
 
 import (
@@ -38,7 +37,6 @@ func NewHTTP(
 	e.HideBanner = true
 	e.HidePort = true
 
-	// Middleware
 	e.Use(middleware.Recover())
 	e.Use(middleware.RequestID())
 	e.Use(myMiddleware.EchoLogger())
@@ -59,50 +57,41 @@ func NewHTTP(
 	}
 }
 
-// Start запускает HTTP сервер
 func (s *HTTPServer) Start() error {
 	addr := fmt.Sprintf(":%s", s.config.Server.Port)
 	logger.Get().Info().Msgf("Starting HTTP server on %s", addr)
 	return s.echo.Start(addr)
 }
 
-// Shutdown graceful shutdown HTTP сервера
 func (s *HTTPServer) Shutdown(ctx context.Context) error {
 	logger.Get().Info().Msg("Shutting down HTTP server...")
 	return s.echo.Shutdown(ctx)
 }
 
-// SetValidator устанавливает валидатор для Echo
 func (s *HTTPServer) SetValidator(v echo.Validator) {
 	s.echo.Validator = v
 }
 
-// GetEcho возвращает экземпляр Echo (для регистрации роутов)
 func (s *HTTPServer) GetEcho() *echo.Echo {
 	return s.echo
 }
 
-// GetServerService возвращает сервис серверов
 func (s *HTTPServer) GetServerService() *service.ServerService {
 	return s.serverService
 }
 
-// GetTaskService возвращает сервис задач
 func (s *HTTPServer) GetTaskService() *service.TaskService {
 	return s.taskService
 }
 
-// GetSyncService возвращает сервис синхронизации
 func (s *HTTPServer) GetSyncService() *service.SyncService {
 	return s.syncService
 }
 
-// GetDB возвращает подключение к БД
 func (s *HTTPServer) GetDB() *postgres.DB {
 	return s.db
 }
 
-// GetEncryptor возвращает шифровальщик
 func (s *HTTPServer) GetEncryptor() *encryption.Encryptor {
 	return s.encryptor
 }
